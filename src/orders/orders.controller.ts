24201b008b3f32b3a56f10abb4e2c1fd046ee684
Nicolas Ortega <nicolasortegabustos@gmail.com>
2024-09-03 14:54:19 -0400

    first comment

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,79 @@
+import { Controller, Get, Post, Body, Param, Inject, ParseUUIDPipe, Query, Patch } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { NATS_SERVICE, ORDER_SERVICE } from 'src/config/services';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import { OrderPaginationDto, StatusDto } from './dto';
+import { PaginationDto } from 'src/common';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy
+  ) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.client.send( 'createOrder', createOrderDto )
+  }
+
+  @Get()
+  findAll(@Query() orderPaginationDto : OrderPaginationDto) {
+    return this.client.send('findAllOrders', orderPaginationDto)
+  }
+
+  @Get('id/:id')
+ async findOne(@Param('id', ParseUUIDPipe) id: string) {
+
+    try {
+      const order = await firstValueFrom(
+        this.client.send('findOneOrder', { id })
+      );
+
+      return order
+       
+    } catch (error) {
+      
+      throw new RpcException(error)
+    }
+    
+  }
+
+  @Get(':status')
+ async findAllByStatus(
+  @Param() statusDto: StatusDto,
+  @Query() paginationDto : PaginationDto
+  ) {
+    try {
+
+     
+      return this.client.send('findAllOrders', {
+        ...paginationDto,
+        status: statusDto.status,
+      });
+
+       
+    } catch (error) {
+      
+      throw new RpcException(error)
+    }
+    
+  }
+
+
+  @Patch(':id')
+  changeStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() statusDto : StatusDto
+
+  ){
+    try {
+      return this.client.send('changeOrderStatus', {id, status : statusDto.status})
+    } catch (error) {
+      throw new RpcException(error);
+      
+    }
+  }
+
+
+}
---
+++ src/orders/orders.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ORDER_SERVICE } from 'src/config/services';
+import { envs } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
+
+@Module({
+  controllers: [OrdersController],
+  providers: [],
+  imports: [NatsModule
+    // ClientsModule.register([
+    //   { name: ORDER_SERVICE, 
+    //     transport: Transport.TCP,
+    //     options: {
+    //       // host: envs.ordersMicroserviceHost,
+    //       // port: envs.ordersMicroservicePort
+    //     } 
+    //   },
+    // ]),
+  ]
+
+})
+export class OrdersModule {}
